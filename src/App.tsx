e208a9db2bc451f5886bba3619698be945b18c8b
brett.cunningham <brett.cunningham@sbevision.com>
2025-05-26 11:15:12 -0400

    fix(html): isPaid data

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,8 @@ function App() {
                               <td> {data["dueDate"]}</td>
                               <td> {data["category"]}</td>
                               <td> {data["source"]}</td>
-                              <td> {data["isPaid"]}</td>
+                              <td> {data["isPaid"] ? "Yes" : "No"} </td>
+                              <button>Edit</button>
                           </tr>
                       })}
                       </tbody>
@@ -165,30 +166,6 @@ function App() {
           </div>
 
       </>
-
-
-      // <>
-      //   <div>
-      //     <a href="https://vite.dev" target="_blank">
-      //       <img src={viteLogo} className="logo" alt="Vite logo" />
-      //     </a>
-      //     <a href="https://react.dev" target="_blank">
-      //       <img src={reactLogo} className="logo react" alt="React logo" />
-      //     </a>
-      //   </div>
-      //   <h1>Vite + React</h1>
-      //   <div className="card">
-      //     <button onClick={() => setCount((count) => count + 1)}>
-      //       count is {count}
-      //     </button>
-      //     <p>
-      //       Edit <code>src/App.tsx</code> and save to test HMR
-      //     </p>
-      //   </div>
-      //   <p className="read-the-docs">
-      //     Click on the Vite and React logos to learn more
-      //   </p>
-      // </>
   )
 }
 
